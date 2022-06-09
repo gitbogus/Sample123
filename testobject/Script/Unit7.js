f5b32b7d96159af80c5362f6901b2f7487cb0587
gitbogus <bogus0115@naver.com>
2022-06-09 16:53:25 +0900

    First Commit

---
+++ testobject/Script/Unit7.js
@@ -0,0 +1,11 @@
+﻿function Test4()
+{
+  //Aliases.browser.pageSinglexProcurement.headerXmlview1PageHeader.buttonXmlview1Pagesearchbutton.ClickButton();
+  //Performs a single click on the specified button.
+  var PropNames, PropValues, LastResult;
+  PropNames = new Array("id");
+  PropValues = new Array("*");
+  LastResult = Aliases.browser.pageSinglexProcurement.headerXmlview1PageHeader.buttonXmlview1Pagesearchbutton.Find(PropNames, PropValues, 1000, true).ClickButton();
+  //Returns the number of milliseconds passed after starting or resetting the specified performance counter.
+
+}
